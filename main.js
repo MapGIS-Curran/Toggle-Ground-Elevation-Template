fbc12be0babd1b7a60842db8b51ae967f059db69
MapGIS <9409101+MapGIS-Curran@users.noreply.github.com>
2019-12-26 01:44:06 -0900

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-require(["esri/Map", "esri/views/SceneView"], function(Map, SceneView) {
-        // Create the Map
-        var map = new Map({
-          basemap: "hybrid",
-          ground: "world-elevation"
-        });
+require(["esri/Map", "esri/views/SceneView"], 
+function(Map, SceneView) {
+  // Create the Map
+  var map = new Map({
+    basemap: "hybrid",
+    ground: "world-elevation"
+  });
 
-        // Create the SceneView
-        var view = new SceneView({
-          container: "viewDiv",
-          map: map,
-          camera: {
-            position: [-150.841, 62.633, 5000],
-            tilt: 80
-          }
-        });
+  // Create the SceneView
+  var view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    camera: {
+      position: [-150.841, 62.633, 5000],
+      tilt: 80
+    }
+  });
 
-        view.when(function() {
-          document
-            .getElementById("elevationInput")
-            .addEventListener("change", updateElevation);
+  view.when(function() {
+    document
+      .getElementById("elevationInput")
+      .addEventListener("change", updateElevation);
 
-          function updateElevation(ev) {
-            // Turn ground layers visibility on/off
-            map.ground.layers.forEach(function(layer) {
-              layer.visible = ev.target.checked;
-            });
-          }
-        });
+    function updateElevation(ev) {
+      // Turn ground layers visibility on/off
+      map.ground.layers.forEach(function(layer) {
+        layer.visible = ev.target.checked;
       });
+    }
+  });
+});
